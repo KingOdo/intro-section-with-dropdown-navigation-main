895fdb642ff3d750908d38c7e0934fdee78473c5
King Odo <Ogblowoodo@gmail.com>
2025-03-29 15:28:51 +0200

    fixed company links

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 
-document.getElementById('company-link').addEventListener('click', (event) =>{
+const arrowDown1=document.getElementById('arrow-down1');
+const arrowUp1=document.getElementById('arrow-up1');
+const arrowDown = document.getElementById('arrow-down');
+const arrowUp = document.getElementById('arrow-up');
+// Company dropdown functionality
+document.getElementById('company-link').addEventListener('click', (event) => {
   event.preventDefault();
-  
-  const companyDropdown= document.querySelector('.company-dropdown');
-  const arrowDown1= document.getElementById('arrow-down1');
-  const arrowUp1= document.getElementById('arrow-up1');
 
-  if(companyDropdown.style.display === '' || companyDropdown.style.display==='none'){
-    console.log("company clicked");
-    companyDropdown.style.display='block';
-    console.log(arrowDown1);
-    arrowDown1.style.display='none';
-    arrowDown1.style.display='block';
-    arrowUp1.style.display='block';
-  } 
-  else{  
-    companyDropdown.style.display='none';
-    arrowDown1.style.display='block';
-    arrowUp1.style.display='none';
+  const companyDropdown = document.querySelector('.company-dropdown');
+
+  if (companyDropdown.style.display === '' || companyDropdown.style.display === 'none') {
+    companyDropdown.style.display = 'block';
+    arrowDown1.style.display = 'none';
+    arrowUp1.style.display = 'inline';
+  } else {
+    companyDropdown.style.display = 'none';
+    arrowDown1.style.display = 'inline';
+    arrowUp1.style.display = 'none';
   }
+});
 
+// Features dropdown functionality
+document.getElementById('features-link').addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const featureDropdown = document.querySelector('.feature-dropdown');
+
+  if (featureDropdown.style.display === '' || featureDropdown.style.display === 'none') {
+    featureDropdown.style.display = 'inline';
+    arrowDown.style.display = 'none';
+    arrowUp.style.display = 'inline';
+  } else {
+    featureDropdown.style.display = 'none';
+    arrowDown.style.display = 'inline';
+    arrowUp.style.display = 'none';
+  }
 });
